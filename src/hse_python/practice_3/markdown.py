f7d3445eb9d07b2012d4af36c65163aa4d61284d
alex-shishkin <alex-shishkin@yandex-team.ru>
2022-10-21 02:26:42 +0300

    added logger decorator

--- src/hse_python/practice_3/markdown.py
+++ src/hse_python/practice_3/markdown.py
@@ -20,13 +20,14 @@ from typing import List
 
 from hse_python.practice_3.formatter import generate_task_markdown, generate_package_markdown
 from hse_python.practice_3.parser import parse_source
-from hse_python.practice_3.utils import Task
+from hse_python.practice_3.utils import Task, measure_time
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 logger = logging.getLogger('markdown')
 
 
+@measure_time(logger=logger)
 def to_markdown(input_filepath: str, output_filepath: str) -> None:
     if output_filepath is None:
         raise ValueError('File markdown transform require output file name')
@@ -57,6 +58,7 @@ def parse_package(input_package_path) -> List[Task]:
     return tasks
 
 
+@measure_time(logger=logger)
 def to_markdown_package(input_package_path: str, output_filepath: str = None) -> None:
     if output_filepath is None:
         output_filepath = os.path.join(input_package_path, 'README.md')
--- src/hse_python/practice_3/utils.py
+++ src/hse_python/practice_3/utils.py
@@ -1,7 +1,21 @@
-from typing import NamedTuple
+import time
+from logging import Logger
+from typing import NamedTuple, Callable
 
 
 class Task(NamedTuple):
     title: str
     description: str
     code: str
+
+
+def measure_time(logger: Logger):
+    def logged_function(func: Callable):
+        def wrapped(*args):
+            start = time.time()
+            res = func(*args)
+            end = time.time()
+            logger.info(f"Function `{func.__name__}` took " + str(round((end - start) * 1000)) + " ms")
+            return res
+        return wrapped
+    return logged_function
